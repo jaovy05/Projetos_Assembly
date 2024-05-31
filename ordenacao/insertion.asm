2e1c104286308f7ab27c047d82349ca1c2ae8826
jaovy05 <joaovitorg.silva050@gmail.com>
2024-05-31 04:14:11 -0300

    :sparkles: insertionSort implementado

--- ordenacao/insertion.asm
+++ ordenacao/insertion.asm
@@ -1,15 +1,14 @@
 		.data
-vetor:		.space 200
-tam:		.asciz "Digite o tamanho do vetor (maximo 50):\n" 
-digite:		.ascii "Digite o "
-numero:		.ascii "01"
-dovetor:	.asciz "° número: "
+
+tam:		.word 50
+vetor:		.space 200 (50 * 4)
 
 		.text
 main:
 	jal set_vector #faz um jump para função set_vector
 	la a0, vetor
 	jal selection_sort #faz um jump para função buble_sort
+	la a0, vetor
 	jal print #faz um jump para função print
 
 fim:
@@ -17,7 +16,7 @@ fim:
 	li a0, 0 #encerra o programa com 0
 	ecall
 	
-#---------- função bubbleSort(*v, n) --------
+#---------- função selectionSort(*v, n) --------
 
 selection_sort:
 	mv s1, a1 		#cópia o tamanho para s1
@@ -28,9 +27,38 @@ for0:
 	beq t0, s1, fim_func
 	mv t1, t0 		# coloca o valor da variavel temporaria em t1
 	addi t2, t0, 1		# variavel de controle j = i + 1
-	lw s0, 0(a0)
+	
+	lw t3, 0(a0)
+	mv s0, a0		# Duplica o vetor min para ter o J
+	addi s0, s0, 4		# J vai ser a proxima casa
+	
 for1:
-	beq t2, a1, troca
+	lw t4, 0(s0)		# Carrega o valor do v[j]
+	beq t2, a1, troca	# Se J for = tamanho vai verificar troca
+	bge t4, t3, incrementa0	# Se o j for maior que o min volta pro loop
+	mv t3, t4		# Troca os valores para min ficar com o menor
+	mv t1, t2		# Atualiza o indice do min
+
+incrementa0:
+	addi t2, t2, 1		# Incrementa j (j++)
+	addi s0, s0, 4		# Incrementa o vetor para acompanhar j, v2 = v[j]
+	j for1
+
+troca:
+	beq t1, t0, incrementa1	# Se for igual volta para o loop
+	sub t1, t1, t0		# Calcula a distância dos inteiros
+	slli t1, t1, 2		# Calcula a distância em bytes
+	lw t2, 0(a0)		# Lê o valor da posição "i" a ser trocada
+	sw t3, 0(a0)		# Troca o i pelo min
+	add a0, a0, t1		# Soma a distância calculada
+	sw t2, 0(a0)		# Troca o min pelo i
+	sub a0, a0, t1		# Volta para a posição normal
+	
+incrementa1:
+	addi t0, t0, 1		# Incrementa i (i++)
+	addi a0, a0, 4		# Incrementa o vetor para acompanhar i, v1 = v[i]
+	j for0			# Volta para o loop principal
+
 	
 #-------- função printf(*v, n) ---------------
 
