776e5df4c195d997527bb9e19ac63abbaf040071
jaovy05 <joaovitorg.silva050@gmail.com>
2024-05-31 05:47:07 -0300

    :fire: função set_vector removida

--- ordenacao/insertion.asm
+++ ordenacao/insertion.asm
@@ -1,14 +1,20 @@
+
 		.data
 
-tam:		.word 50
-vetor:		.space 200 (50 * 4)
+tam:		.word 100
+vetor:		.space 400 # (tam* 4)
 
 		.text
+
 main:
-	jal set_vector #faz um jump para função set_vector
 	la a0, vetor
+	lw a1, tam
+	call set_vector #faz um jump para função set_vector
+	la a0, vetor
+	lw a1, tam
 	jal selection_sort #faz um jump para função buble_sort
 	la a0, vetor
+	lw a1, tam
 	jal print #faz um jump para função print
 
 fim:
@@ -82,58 +88,7 @@ for_print:
 	addi t1, t1, 1
 	j for_print	
 	
-#-------- função set_vector() -----------------------
 
-set_vector:
-	li a7, 4 #chamada de sistema de código 4 (printar string)
-	la a0, tam #insere o valor do primeiro endereço de memória que está o vetor tam no registrador a0 
-	ecall
-	
-	li a7, 5 #chamada de sistema de código 5 (ler um inteiro)
-	ecall
-	mv a1, a0 #coloca o valor do inteiro que está em a0 no registrador a1
-	la t0, vetor #insere o valor do primeiro endereço de memória do vetor no registrador t0
-	mv t1, zero #o registrador t1 ta recebendo o valor 0
-	
-for_set_vector:
-	bge t1, a1, fim_func #verifica se t1 >= a1, caso seja verdadeiro vai para fim_func
-	
-	li a7, 4 #chamada de sistema de código 4 (printar string)
-	la a0, digite #insere o valor do primeiro endereço de memória que está o vetor digite no registrador a0 
-	ecall
-	
-	li a7, 5 #chamada de sistema de código 5 (ler um inteiro)
-	ecall
-	#pega o numero digitado e coloca no vetor
-	sw a0, 0(t0)
-	
-	mv t6, ra
-	jal incrementar
-	mv ra, t6
-	
-	addi t0, t0, 4 #soma 4 no registrador t0
-	addi t1, t1, 1 #soma 1 no registrador t1
-	
-	j for_set_vector #faz um jump na função for set_vector
-	
-#----------------------------- FUNÇÃO INCREMENTAR ------------------------------------------
-
-incrementar:
-	la t2, numero #insere o valor do primeiro endereço de memória do vetor numero no registrador t2
-	lb t3, 1(t2) #o registrador t3 recebe a unidade 
-	addi t3, t3, 1 #soma 1 ao valor do registrador t3
-	li t4, 58 #58 é o proximo valor de 9 na tabl
-	beq t3, t4, zerar #se t3 = t4 então t3 estorou os inteiros da tabela ascii (0 - 9)
-	sb t3, 1(t2) #coloca a unidade no respectivo endereço
-	ret #fim da função
-
-zerar:
-	addi t3, t3, -10 #agora t3 volta 10 caracteres na tabela ascii (t3 = 0)
-	lb t4, 0(t2) #o registrador t4 recebe a dezena
-	addi t4, t4, 1 #soma 1 à dezena
-	sb t3, 1(t2) #coloca a unidade no respectivo endereço
-	sb t4, 0(t2) #coloca a dezena no respectivo endereço
-	
 fim_func:
 	ret #retorna ao endereço de memória que esta no registrador ra
 
