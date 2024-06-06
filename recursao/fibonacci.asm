0c885a7c8d43b3a468991d66a638bee368e1285e
jaovy05 <joaovitorg.silva050@gmail.com>
2024-06-06 17:55:08 -0300

    :sparkles: Lado direito aparentemente correto

--- recursao/fibonacci.asm
+++ recursao/fibonacci.asm
@@ -7,16 +7,20 @@ fibonacci:
 	ble a0, t0, fim
 	
 	addi sp, sp, -8
-	sw ra, (sp)
-	sw a0, 4(sp)
-	# | 0Xffff |
-	# |    i   |	
+	sw a0, (sp)
+	sw ra, 4(sp)
+	# |  i   |
+	# |  fff |	
 	addi a0, a0, -1
 	jal fibonacci
 	mv t1, a0
-	lw a0, -4(sp)
+	lw a0, (sp)
+	addi sp, sp, 4 #desempilha o numero
+	addi a0, a0, -2
 	jal fibonacci
 	add a0, a0, t1
+	lw ra, (sp)
+	addi sp, sp, 4
 	ret
 	
 fim:
