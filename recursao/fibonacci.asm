dc4934eac64b8b65374d41655b15a6becb54455d
jaovy05 <joaovitorg.silva050@gmail.com>
2024-07-16 03:13:08 -0300

    fibonacci

--- recursao/fibonacci.asm
+++ recursao/fibonacci.asm
@@ -1,28 +1,29 @@
-#função fibonacci:
-# input: a0 = numero da seguencia
-# output: a0 = valor
 .global fibonacci
 fibonacci:
 	li t0, 2
-	ble a0, t0, fim
-	
-	addi sp, sp, -8
-	sw a0, (sp)
-	sw ra, 4(sp)
-	# |  i   |
-	# |  fff |	
-	addi a0, a0, -1
-	jal fibonacci
-	mv t1, a0
-	lw a0, (sp)
-	addi sp, sp, 4 #desempilha o numero
-	addi a0, a0, -2
-	jal fibonacci
-	add a0, a0, t1
-	lw ra, (sp)
-	addi sp, sp, 4
-	ret
-	
+	ble a0, t0, fim        # se a entrada for menor que 2 retorna 1 
+
+	addi sp, sp, -12       # empilha 3 posições de memória
+	sw a0, 8(sp)           # salva a0 na pilha
+	sw ra, 4(sp)           # salva ra na pilha
+	sw a1, 0(sp)           # salva s0 na pilha
+
+	addi a0, a0, -1        # a0 = a0 - 1
+	jal fibonacci          # chamada recursiva fibonacci(n-1)
+	mv a1, a0              # salva o resultado de fibonacci(n-1) em s0
+
+	lw a0, 8(sp)           # restaura o valor original de a0
+	addi a0, a0, -2        # a0 = a0 - 2
+	jal fibonacci          # chamada recursiva fibonacci(n-2)
+
+	add a0, a0, a1         # soma os resultados de fibonacci(n-1) e fibonacci(n-2)
+
+	lw ra, 4(sp)           # restaura ra da pilha
+	lw a1, 0(sp)           # restaura s0 da pilha
+	addi sp, sp, 12        # ajusta a pilha de volta
+
+	ret                    # retorna
+
 fim:
-	li a0, 1
+	li a0, 1               # retorna 1 para fibonacci(0) e fibonacci(1)
 	ret
